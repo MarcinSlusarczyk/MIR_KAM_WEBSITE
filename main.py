44fbdd0956fe1f4ca874ec95db37d401e198dc59
MarcinSlusarczyk <marcink442@gmail.com>
2022-02-19 11:45:45 +0100

    Initial commit

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('main.html')
+
+@app.route('/cennik_nagrobki')
+def cennik_nagrobki():
+    return render_template('cennik_nagrobki.html')
 
 
 if __name__ == '__main__':
